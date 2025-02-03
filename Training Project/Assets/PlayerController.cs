e30e7838b3ed0d4ed925e2c7f1a33f62b46e5fde
Jdevey352 <jakeydevey@gmail.com>
2025-02-03 13:51:40 -0500

    Update PlayerController.cs

--- Training Project/Assets/PlayerController.cs
+++ Training Project/Assets/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector2(3, -2);
     }
 
     // Update is called once per frame
     void Update()
     {
+
         
     }
 }
