4f4d0861b7b849ba4863a74d998b5faeef7c128a
Jdevey352 <jakeydevey@gmail.com>
2025-02-14 12:58:46 -0500

    changes

--- Training Project/Assets/Scripts/PlayerController.cs
+++ Training Project/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     private PlayerInput _input; //field to reference Player Input component
     private Rigidbody2D _rigidbody;
+    //add this to reference a prefab that is set in the inspector
+    public GameObject ballPrefab;
+
+    //facing direction (even after stopped)
+    private Vector2 _facingVector = Vector2.right;
 
     // Start is called before the first frame update
     void Start()
@@ -30,10 +35,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if Fire action was performed log it to the console
         if (_input.actions["Fire"].WasPressedThisFrame())
         {
-            Debug.Log("Fire activated!");
+            //create a new object that is a clone of the ballPrefab
+            //at this object's position and default rotation
+            //and use a new variable (ball) to reference the clone
+            var ball = Instantiate(ballPrefab,
+                                transform.position,
+                                Quaternion.identity);
+            //Get the Rigidbody 2D component from the new ball 
+            //and set its velocity to x:-10f, y:0, z:0
+            ball.GetComponent<Rigidbody2D>().velocity = Vector2.left * 10f;
+
+            //call SetDirection from BallController on new ball
+            ball.GetComponent<BallController>()?.SetDirection(_facingVector);
+
         }
     }
 
@@ -44,6 +60,13 @@ public class PlayerController : MonoBehaviour
 
         //change the velocity to match the Move (every physics update)
         _rigidbody.velocity = dir * 5;
+
+        //to keep track of facing for aiming when stopped:
+        //Set _facingVector only while controls are moving
+        //(digital only gives 8 directions, analog is nicer)
+        if (dir.magnitude > 0.5) {
+            _facingVector = _rigidbody.velocity;
+        }
     }
 }
 
